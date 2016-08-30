4f6f69d5e9a9a73fbb87ed3929f4a72598d76f6c
mikenina <mikkola@list.ru>
2016-08-30 20:02:01 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -34,3 +34,76 @@ $data = [
         ]
     ],
 ];
+
+/**
+ * @param $data
+ * @return array
+ */
+function func_data1_to_data($data)
+{
+    $return = [];
+    foreach ($data as $key => $val) {
+
+        _parse_row($key, $val, $return);
+
+    }
+    return $return;
+}
+
+/**
+ * @param $key
+ * @param $val
+ * @param $tmp
+ */
+function _parse_row($key, $val, &$tmp)
+{
+    if (($pos = strpos($key, '.')) !== false) {
+
+        $key_part = substr($key, 0, $pos);
+        $key_rest = preg_replace('/([0-9a-z]+\.)/', '', $key, 1);
+
+        if (!isset($tmp[$key_part])) {
+            $tmp[$key_part] = [];
+        }
+        _parse_row($key_rest , $val, $tmp[$key_part]);
+
+    } else {
+        $tmp[$key] = $val;
+        return;
+    }
+}
+
+print_r(func_data1_to_data($data1));
+
+/**
+ * @param $data
+ * @return mixed
+ */
+function func_data_to_data1($data)
+{
+    static $return = [];
+    static $keys = [];
+    static $level = 0;
+
+    $level++;
+
+    foreach ($data as $key => $val) {
+
+        $keys[$level] = $key;
+        if (is_array($val)) {
+
+            func_data_to_data1($val);
+
+        } else {
+            $return[implode('.', $keys)] = $val;
+        }
+
+        unset($keys[$level]);
+    }
+
+    $level--;
+
+    return $return;
+}
+
+print_r(func_data_to_data1($data));
